f3776ad14c8329c27de51b8431e5563af1a34af2
Administrator <swati.palkar@jpmorgan.com>
2018-12-05 15:19:26 +0530

    commit

---
+++ JPAHibernate/JPM_134_JPA_Association_MTM_BI/src/com/jpm/mtm/client/MTMClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jpm.mtm.client;
+
+/**
+ * @author Administrator
+ *
+ */
+public class MTMClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

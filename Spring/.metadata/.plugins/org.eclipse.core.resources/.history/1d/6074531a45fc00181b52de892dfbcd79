4731bf093bc9bf9eec31dd4a8d6c04ad78ff2a9b
Administrator <swati.palkar@jpmorgan.com>
2018-12-10 16:16:21 +0530

    commit mid

---
+++ Spring/.metadata/.plugins/org.eclipse.core.resources/.history/1d/6074531a45fc00181b52de892dfbcd79
@@ -0,0 +1,14 @@
+package com.java;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpringFactory {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/java/context.xml"); //This is the path of the configuration file
+
+	}
+
+}
---
+++ Spring/Spring020_moreBasics/src/main/java/com/jpm/hr/services/MyApplServices.java
@@ -0,0 +1,53 @@
+package com.jpm.hr.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.jpm.hr.daos.MyApplDao;
+
+/*When bean alias (service) is not mentioned, the default is class name in camel case.
+ * 
+ * @Component has the below subcomponent
+ * 	@Service - for declaring service beans
+ * 	@Repository - for declaring dao beans.
+ * 	@Controller - for Controller in spring MVC
+ * 	@RestController - for declaring rest service layer
+ * 
+ */
+
+//@Component("service")
+@Service("service")
+@Lazy(true)
+@Scope("singleton")
+public class MyApplServices {
+private MyApplDao dao;
+	
+	public MyApplServices() {
+		
+		//dao is reference and new MyApplDao is object created
+		//This code owns a responsibility of creating an object and holding a reference
+		//dao = new MyApplDao();
+		System.out.println("OBject of class MyApplSErvice is created");
+	}
+	
+	// Dependency Injection using Constructor - Constructor will not create the object
+	//@Autowired //Auto wiring by type/plumbing
+	public MyApplServices(MyApplDao dao){
+		this.dao = dao;
+		System.out.println("OBject of class MyApplSErvice(dao) is created");
+	}
+	
+	//Setter injection. when we resolve the dependency through setter method
+	@Autowired
+	public void setDao(MyApplDao dao) {
+		this.dao = dao;
+		System.out.println("In setter for injecting dao");
+	}
+	
+	
+	
+
+}

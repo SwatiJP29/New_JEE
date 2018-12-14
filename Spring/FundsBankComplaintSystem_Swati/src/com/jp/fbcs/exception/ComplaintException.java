a46ae02542bd29f57645dbf7604d5a2cb5ebf7f7
Administrator <swati.palkar@jpmorgan.com>
2018-12-14 16:06:36 +0530

    commit first

---
+++ Spring/FundsBankComplaintSystem_Swati/src/com/jp/fbcs/exception/ComplaintException.java
@@ -0,0 +1,15 @@
+package com.jp.fbcs.exception;
+
+public class ComplaintException extends Exception {
+
+	public ComplaintException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ComplaintException(String message) {
+		super(message);
+	}
+	
+	
+
+}
